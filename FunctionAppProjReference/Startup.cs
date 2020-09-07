2807c9e9a35ada684bdcf76e11f5fd9979007db6
Taras B <Taras.Buha@gmail.com>
2020-09-07 13:21:34 +0300

    from nuget

--- FunctionAppProjReference/Startup.cs
+++ FunctionAppProjReference/Startup.cs
@@ -1,18 +1,18 @@
-﻿using Genesys.Azure.WebJobs.Extensions;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+﻿//using Genesys.Azure.WebJobs.Extensions;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Hosting;
+//using Microsoft.Extensions.DependencyInjection;
 
-[assembly: WebJobsStartup(typeof(FunctionAppProjReference.Startup))]
-namespace FunctionAppProjReference
-{
-    internal class Startup : IWebJobsStartup
-    {
-        public void Configure(IWebJobsBuilder builder)
-        {
-            //builder.Services.AddSingleton<GenesysTokenProvider>();
-            //builder.AddExtension<GenesysAttributeCustomConfigProvider>();
-            builder.AddExtension<GenesysAttributeConfigProvider>();
-        }
-    }
-}
+//[assembly: WebJobsStartup(typeof(FunctionAppProjReference.Startup))]
+//namespace FunctionAppProjReference
+//{
+//    internal class Startup : IWebJobsStartup
+//    {
+//        public void Configure(IWebJobsBuilder builder)
+//        {
+//            //builder.Services.AddSingleton<GenesysTokenProvider>();
+//            //builder.AddExtension<GenesysAttributeCustomConfigProvider>();
+//            builder.AddExtension<GenesysAttributeConfigProvider>();
+//        }
+//    }
+//}
